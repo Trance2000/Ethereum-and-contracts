423992fae6a9b0b4cfbad17faa4ea5f18c093b67
Artoym Toupchienko <artoymt1990@gmail.com>
2018-01-29 18:01:53 -0500

    chnaged to lottery contract

--- compile.js
+++ compile.js
@@ -2,9 +2,9 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc'); // solidity compiler installed via npm
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); // location of the contract
-const source = fs.readFileSync(inboxPath, 'utf8'); // reading code from contract in utf8
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol'); // location of the contract
+const source = fs.readFileSync(lotteryPath, 'utf8'); // reading code from contract in utf8
                                   //  ^ - the number of different contracts that we would like to compile
-module.exports = solc.compile(source, 1).contracts[':Inbox']; // exporting the compiled code from the read Inbox.sol file
+module.exports = solc.compile(source, 1).contracts[':Lottery']; // exporting the compiled code from the read Inbox.sol file
                                                               // specifically the 'Inbox' contract that is located insude the
                                                               // 'contracts' object
--- deploy.js
+++ deploy.js
@@ -14,7 +14,7 @@ const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
     console.log('Contract deployed to following address:', result.options.address);
