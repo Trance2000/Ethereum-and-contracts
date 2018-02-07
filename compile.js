eec7c8ff9d6968dd281758e6f82ae5b80cf5791e
Artoym Toupchienko <artoymt1990@gmail.com>
2018-02-07 15:30:26 -0500

    fixed some errors caused by typos

--- compile.js
+++ compile.js
@@ -4,7 +4,7 @@ const solc = require('solc'); // solidity compiler installed via npm
 
 const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol'); // location of the contract
 const source = fs.readFileSync(lotteryPath, 'utf8'); // reading code from contract in utf8
-                                  //  ^ - the number of different contracts that we would like to compile
+                                  //  | - the number of different contracts that we would like to compile
 module.exports = solc.compile(source, 1).contracts[':Lottery']; // exporting the compiled code from the read Inbox.sol file
-                                                              // specifically the 'Inbox' contract that is located insude the
+                                                              // specifically the 'Lottery' contract that is located inside the
                                                               // 'contracts' object
